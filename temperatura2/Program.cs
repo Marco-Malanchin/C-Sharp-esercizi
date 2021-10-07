2e0096fefa028666b26e15dbd7d171243fe5c018
Marco-Malanchin <72445677+Marco-Malanchin@users.noreply.github.com>
2021-10-07 09:44:51 +0200

    EX 6

---
+++ temperatura2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace temperatura2
+{
+    class temperatura2
+    {
+        //attributi
+        int t, min, max;
+        //costuttore
+        public temperatura2(int t)
+        {
+            this.t = t;
+            min = 9999;
+            max = -9999;
+        }
+        //metodi
+        public void setTemp()
+        {
+            Console.WriteLine("Inserisci le temperatura");
+            t = int.Parse(Console.ReadLine());
+        }
+
+        public int minimo()
+        {
+            if (min > t)
+                min = t;
+            return min;
+        }
+        public int massimo()
+        {
+            if (max < t)
+                max = t;
+            return max;
+        }
+
+        static void Main(string[] args)
+        {
+            int n, t = 0;
+            temperatura2 k = new temperatura2(t);
+            Console.WriteLine("Quante temperature vuoi inserire?");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                k.setTemp();
+                k.minimo();
+                k.massimo();
+            }
+            Console.WriteLine("La temperatura massima e' : {0}, quella minima e' : {1}", k.massimo(), k.minimo());
+            Console.ReadKey();
+        }
+    }
+}
